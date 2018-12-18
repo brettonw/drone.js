10f83270514b7821ecaa95548710bcfd54ac7d5a
Bretton Wade <brettonw.wade@gmail.com>
2018-12-18 08:09:26 -0500

    checkgoal wasn't actually returning true...

--- js/drone-tester.js
+++ js/drone-tester.js
@@ -11,23 +11,28 @@ let DroneTester = function () {
     };
 
     const span = 15.0;
-    const goalSpacingSq = 100;
+    const goalSpacingSq = 40;
     const goalSpacing = Math.sqrt (goalSpacingSq);
     const yOffset = 1.5;
 
-    let checkGoal = function (from, toGoal) {
-        let deltaToGoal = Float3.subtract (toGoal, from);
+    _.checkGoal = function (goal) {
+        let result = false;
+
+        let from = this.goal;
+        let deltaToGoal = Float3.subtract (goal, from);
         let distanceToGoal = Float3.norm (deltaToGoal);
 
         // check that the distance to the goal is sufficient
         if (distanceToGoal > (span * 1.25)) {
+            result = true;
 
+            let that = this;
             DroneThing.forEach (function (thing) {
-                if ("goal" in thing) {
+                if ((thing != that) && (result === true) && ("goal" in thing)) {
                     // check if thing.goal is too close to our goal
-                    let deltaGoals = Float3.subtract (thing.goal, toGoal);
+                    let deltaGoals = Float3.subtract (thing.goal, goal);
                     if (Float3.normSq (deltaGoals) < goalSpacingSq) {
-                        return false;
+                        result = false;
                     }
                 }
             });
@@ -36,8 +41,10 @@ let DroneTester = function () {
             // normalize the delta
             deltaToGoal = Float3.scale (deltaToGoal, 1 / distanceToGoal);
 
+            // go over all the drones... (except this one)
+            let that = this;
             DroneThing.forEach (function (thing) {
-                if ("goal" in thing) {
+                if ((thing != that) && (result === true) && ("goal" in thing)) {
                     // check if thing.goal is too close to a line between from-to - it's not a
                     // guarantee, but it is a start... start by projecting the goal onto our line
                     let deltaToThingGoal = Float3.subtract  (thing.goal, from);
@@ -47,7 +54,7 @@ let DroneTester = function () {
                     // the thing's goal is behind us and we can ignore it - it should have met these
                     // requirements itself... if it's more than "far" away, we can ignore it too...
                     let dot = Float3.dot (deltaToThingGoal, deltaToGoal);
-                    if ((dot > 0) && (dot < (distanceToGoal + goalSpacing))) {
+                    if ((dot > -goalSpacing) && (dot < (distanceToGoal + goalSpacing))) {
                         // ok, so it's between us and our goal at *some* distance, but how far?
                         let projectedPoint = Float3.add (from, Float3.scale (deltaToGoal, dot));
                         let projectedDelta = Float3.subtract (thing.goal, projectedPoint);
@@ -55,17 +62,14 @@ let DroneTester = function () {
                         if (projectedDeltaLenSq < goalSpacingSq) {
                             // this goal is too close to our path - say no
                             //console.log ("REJECT PATH");
-                            return false;
+                            result = false;
                         }
                     }
                 }
             });
             */
-
-            // we passed all the other tests, go for it
-            return true;
         }
-        return false;
+        return result;
     };
 
     _.update = function (deltaTime) {
@@ -88,7 +92,7 @@ let DroneTester = function () {
                 let newGoal = Float3.copy (this.goal);
 
                 // force the thing to make big moves
-                while (! checkGoal (this.goal, newGoal)) {
+                while (! this.checkGoal (newGoal)) {
                     newGoal[0] = Math.floor (Math.random () * span * 2) - span;
                     newGoal[1] = yOffset + Math.floor (Math.random () * span);
                     newGoal[2] = Math.floor (Math.random () * span * 2) - span;
