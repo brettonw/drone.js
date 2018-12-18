b69087d4544f0b1bc9b8e764dc9a20e73260275d
Bretton Wade <bretton.wade@gmail.com>
2018-12-17 21:02:10 -0500

    tester actually waits until the drone has reached the target to start a countdown.

--- js/drone-tester.js
+++ js/drone-tester.js
@@ -4,31 +4,40 @@ let DroneTester = function () {
     let _ = Object.create (DroneThing);
 
     _.construct = function (parameters) {
-        this.countdownDuration = 8 + (Math.random () * 8);
-        this.countdownTime = 5 + (Math.random () * 5);
+        this.countdownTime = this.countdownDuration = 3 + (Math.random () * 3);
 
         // call the super class constructor
         Object.getPrototypeOf(_).construct.call(this, parameters);
     };
 
     _.update = function (deltaTime) {
-        this.countdownTime -= deltaTime;
-        if (this.countdownTime < 0) {
-            this.countdownTime = this.countdownDuration;
-            /*
-            let controller = navigator.getGamepads()[0];
-            let axes = controller ? controller.axes : [0, 0, 0, 0];
-            */
-            let newGoal = Float3.copy (this.goal);
-            let radius = 15.0;
+        // check if the drone is at the goal
+        let distanceToTarget = Float3.norm (Float3.subtract (this.drone.position, this.goal));
+        if (distanceToTarget < 0.1) {
+            // it is, so start counting down until time to leave
+            this.countdownTime -= deltaTime;
 
-            // force the thing to make big moves
-            while (Float3.norm (Float3.subtract (newGoal, this.goal)) < (radius * 1.25)) {
-                newGoal[0] = Math.floor (Math.random () * radius * 2) - radius;
-                newGoal[1] = 1.5 + Math.floor (Math.random () * radius);
-                newGoal[2] = Math.floor (Math.random () * radius * 2) - radius;
+            // check if it's time to leave
+            if (this.countdownTime < 0) {
+                // it is, so reset the countdown
+                this.countdownTime = this.countdownDuration;
+                /*
+                let controller = navigator.getGamepads()[0];
+                let axes = controller ? controller.axes : [0, 0, 0, 0];
+                */
+
+                // establish a new goal
+                const radius = 15.0;
+                let newGoal = Float3.copy (this.goal);
+
+                // force the thing to make big moves
+                while (Float3.norm (Float3.subtract (newGoal, this.goal)) < (radius * 1.25)) {
+                    newGoal[0] = Math.floor (Math.random () * radius * 2) - radius;
+                    newGoal[1] = 1.5 + Math.floor (Math.random () * radius);
+                    newGoal[2] = Math.floor (Math.random () * radius * 2) - radius;
+                }
+                this.goto (newGoal);
             }
-            this.goto (newGoal);
         }
     };
 
